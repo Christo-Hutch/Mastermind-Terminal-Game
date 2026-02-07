95ac9d926d87a854ccdc46fd7b04cf5e5353c037
Christo-Hutch <chris.hutchinson008@gmail.com>
2026-02-07 18:37:02 +0000

    Added call to initialise game before testing

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -7,5 +7,12 @@ project_root = Path(__file__).resolve().parent.parent
 sys.path.append(str(project_root))
 import src.game_logic
 
+# Initialises Game
+src.game_logic.Game.initialiseGame()
+
 # Test Board with 5 rows
 testBoard = src.game_logic.Board(5)
+
+# Sets a random code maker code
+testBoard.setRandomCode()
+
