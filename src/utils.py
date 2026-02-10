b33fc6c5fd19795e522d0bea7b8750b3a792eb64
Christo-Hutch <chris.hutchinson008@gmail.com>
2026-02-10 14:13:33 +0000

    Fixed 'convertUserColourInput' by removing .upper
    I've removed the .upper() function from the method
    as I am using lowercase letters in my dictionary.

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,17 @@
-import game_logic
+import src.game_logic
 
 # Converts a comma-separated string of color names or initials into a list of initials.
 def convertUserColourInput(user_input: str):
-    colour_map = game_logic.Game.getSettingsData("colours")
+    colour_map = src.game_logic.Game.getSettingsData("colours")
     name_to_initial = {name.lower(): initial for initial, name in colour_map.items()}
     
     raw_inputs = [c.strip().lower() for c in user_input.split(",")]
     converted_list = []
 
+    print(name_to_initial)
+
     for item in raw_inputs:
-        initial = item.upper()
+        initial = item
         if initial in colour_map:
             converted_list.append(initial)
 
